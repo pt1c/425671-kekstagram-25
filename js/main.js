28e1547e170abbd1e55b4c8544aa3634cdd73449
pt1c <ravenzone@mail.ru>
2022-02-20 22:52:47 +0300

    Исправляет замечания

--- js/main.js
+++ js/main.js
@@ -1,20 +1,13 @@
 
 function getRandomInt(from, to){
-  let cFrom = parseInt(from, 10);
-  let cTo = parseInt(to, 10);
+  if (from === to) {return from;}
+  if(to<from) {[from, to] = [to, from];}
 
-  if(cFrom < 0 || cTo <0){
-    //error Ошибочный диапозон
-    return '';
-  }
-  if (cFrom === cTo) {return from;}
-  if(cTo<cFrom) {[cFrom, cTo] = [cTo, cFrom];}
-
-  return Math.floor(Math.random() * cTo) + cFrom;
+  return Math.floor(Math.random() * to) + from;
 }
 
 function checkMaxStringLength(inputString, maxLenght){
-  return !(inputString.length > maxLenght);
+  return inputString.length <= maxLenght;
 }
 
 //Тестовые вызовы функций
