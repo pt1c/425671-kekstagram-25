fdbd724fbc58bd698e936b1c7f95cf93dcf92a3a
pt1c <ravenzone@mail.ru>
2022-03-14 13:02:49 +0300

    Исправляет замечания

--- js/render-full.js
+++ js/render-full.js
@@ -1,39 +1,56 @@
 
-const pictures = document.querySelector('.pictures');
+const picture = document.querySelector('.pictures');
 const bigPicture = document.querySelector('.big-picture');
 
-const toggleVisible = () => {
-  bigPicture.classList.toggle('hidden');
-  document.body.classList.toggle('modal-open');
+const toggleVisible = (action) => {
+  if (action === 'open') {
+    bigPicture.classList.remove('hidden');
+    document.body.classList.add('modal-open');
+  } else {
+    bigPicture.classList.add('hidden');
+    document.body.classList.remove('modal-open');
+  }
+};
+
+const renderComments = (comments) => {
+  const commentsElement = bigPicture.querySelector('.social__comments');
+
+  commentsElement.innerHTML = '';
+  comments.forEach((comment) => {
+    const commentListItem = document.createElement('li');
+    commentListItem.classList.add('social__comment');
+
+    const commentImage = document.createElement('img');
+    commentImage.classList.add('social__picture');
+    commentImage.src = comment.avatar;
+    commentImage.alt = comment.name;
+    commentImage.width = 35;
+    commentImage.height = 35;
+
+    const commentMessage = document.createElement('p');
+    commentMessage.classList.add('social__text');
+    commentMessage.textContent = comment.message;
+
+    commentListItem.append(commentImage);
+    commentListItem.append(commentMessage);
+
+    commentsElement.append(commentListItem);
+  });
 };
 
 const renderFull = (data) => {
-  pictures.addEventListener('click', (evt) => {
-    if (evt.target.nodeName === 'IMG') {
-      const clickedPost = data.filter((post) => post.id === Number(evt.target.dataset.photoId))[0];
+  picture.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('picture__img')) {
+      const clickedPost = data.find((post) => post.id === Number(evt.target.dataset.photoId));
 
       bigPicture.querySelector('.big-picture__img').querySelector('img').src = clickedPost.url;
       bigPicture.querySelector('.likes-count').textContent = clickedPost.likes;
       bigPicture.querySelector('.comments-count').textContent = clickedPost.comments.length;
       bigPicture.querySelector('.social__caption').textContent = clickedPost.description;
 
-      const commentsElement = bigPicture.querySelector('.social__comments');
-      commentsElement.innerHTML = '';
-      clickedPost.comments.forEach((comment) => {
-        const htmlContent = `
-            <li class="social__comment">
-                <img
-                    class="social__picture"
-                    src="${comment.avatar}"
-                    alt="${comment.name}"
-                    width="35" height="35">
-                <p class="social__text">${comment.message}</p>
-            </li>
-        `;
-        commentsElement.innerHTML += htmlContent;
-      });
+      renderComments(clickedPost.comments);
 
-      toggleVisible();
+      toggleVisible('open');
 
       bigPicture.querySelector('.social__comment-count').classList.add('hidden');
       bigPicture.querySelector('.comments-loader').classList.add('hidden');
@@ -43,12 +60,12 @@ const renderFull = (data) => {
 
 document.addEventListener('keydown', (evt) => {
   if (evt.key === 'Escape') {
-    toggleVisible();
+    toggleVisible('close');
   }
 });
 
 bigPicture.querySelector('#picture-cancel').addEventListener('click', () => {
-  toggleVisible();
+  toggleVisible('close');
 });
 
 export { renderFull };
